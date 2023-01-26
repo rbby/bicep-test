69e462b830876bcb74334f95bfef99a99076a206
rbby <robbypot@gmail.com>
2023-01-26 14:27:58 +0100

    Update scrapbook.ps1

--- scrapbook.ps1
+++ scrapbook.ps1
@@ -33,6 +33,7 @@ $params = @{
 Write-Output($params)
 New-AzTemplateSpec @params -Force
 
+Remove-AzTemplateSpec -ResourceGroupName $resourcegroupName -Name $name -Force
 
 # Test Deploy
 New-AzResourceGroupDeployment `
@@ -45,54 +46,6 @@ New-AzResourceGroupDeployment `
   -DeploymentName 'azdo-defId_1-runId_29' `
   -ResourceGroupName $resourcegroupName `
   -TemplateSpecId $id
-
-
-
-# Delete
-Get-AzResourceGroup -Tag @{'environment'='test'} | Remove-AzResourceGroup -Force
-
-# Test SA lock
-$resourceGroup = "rg-bla"
-$location = "westeurope"
-Get-AzLocation | Select-Object Location
-New-AzResourceGroup -Name $resourceGroup -Location $location -Tag @{ "environment"="test" }
-New-AzStorageAccount -ResourceGroupName $resourceGroup `
-  -Name robbyz `
-  -Location $location `
-  -SkuName Premium_ZRS `
-  -Kind StorageV2
-
-# Deploy bicep/
-$id = "/subscriptions/11111111-1111-1111-1111-111111111111/resourceGroups/templateSpecsRG/providers/Microsoft.Resources/templateSpecs/storageSpec/versions/1.0a"
-
-New-AzResourceGroupDeployment `
-  -TemplateSpecId $id `
-  -ResourceGroupName demoRG
-
-
-Set-Location E:\repos\Services\PFZW.Web.Services.Particulieren\deploy\azure\arm-templates
-$RG = 'rg-mijnpfzw-hosting-o'
-
-New-AzResourceGroupDeployment `
-  -DeploymentName 'azdo-defId_1-runId_1' `
-  -ResourceGroupName $RG `
-  -TemplateFile 'plan-sa.bicep' `
-  -TemplateParameterFile 'plan-sa.parameters.o.json' 
-
-# Set role assignment
-$servicePrincial = Get-AzADServicePrincipal -SearchString 'azuredevops-rg-mijnpfzw-hosting-o'
-$servicePrincial
-(Get-AzADServicePrincipal -DisplayName 'azuredevops-rg-mijnpfzw-hosting-o').id
-Get-AzRoleDefinition | Format-Table -Property Name, IsCustom, Id
-$role = Get-AzRoleDefinition -Name 'Template Spec Reader'
-$role
-
-$resource = Get-AzResource -Name 'StorageAccounts-pe-storage' -ResourceGroupName 'rg-it4it-o'
-$resource
-
-New-AzRoleAssignment -ObjectId servicePrincial.Id `
--RoleDefinitionName $role.Name `
--Scope $resource.ResourceId
 
 
 # Delete
